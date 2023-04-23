43face7e3e74e4c6513d8b3b4754aee22ceda840
jose <silvapeixotojoseinacio@gmail.com>
2023-04-23 13:22:43 -0300

    fixing bugs

--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -6,26 +6,19 @@
 /*   By: jinacio- < jinacio-@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 16:09:24 by jinacio-          #+#    #+#             */
-/*   Updated: 2023/03/22 18:03:38 by jinacio-         ###   ########.fr       */
+/*   Updated: 2023/04/23 13:22:22 by jinacio-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
-#include<iomanip>
+#include <iomanip>
 
-int main(int argc, char *argv[])
+std::map<std::string, std::string> creatListMap(std::ifstream & excelBase)
 {
 	std::map<std::string,std::string>data;
-	std::ifstream excelBase;
 	std::string ifs;
 	std::string pairs;
 	std::string odd;
-
-	excelBase.open("data.csv", std::ios::in);
-	if (!excelBase)
-		std::cout << "Deu ruim!" << std::endl;
-	else
-		std::cout << "Deu boa!!" << std::endl;
 
 	while(!excelBase.eof())
 	{
@@ -52,14 +45,56 @@ int main(int argc, char *argv[])
 				count = 0;
 			}
 		}
-		//std::cout << "" << ifs << "\n" ;
 	}
+	data.erase("date");
+	return data;
+}
 
+void openNCheck(std::ifstream & excelBase, std::ifstream & inputBase, char *argv[])
+{
+	excelBase.open("data.csv", std::ios::in);
+	if (!excelBase)
+		std::cout << "The Excel file opened successfully" << std::endl;
+	else
+		std::cout << "Error: The Excel file did not open" << std::endl;
+
+	inputBase.open(argv[1], std::ios::in);
+	if (!inputBase)
+		std::cout << "The input file opened successfully" << std::endl;
+	else
+		std::cout << "Error: The input file did not open." << std::endl;
+}
+
+void	printing(std::map<std::string,std::string> & data)
+{
 	std::map<std::string, std::string>::iterator it = data.begin();
+	int i = 0;
 	while (it != data.end())
 	{
-		std::cout << "Key: " << it->first << ", Value: " << it->second << std::endl;
+		std::cout << "Key: " << it->first << ", Value: "
+				<< it->second << std::endl;
 		++it;
+		//i++;
+		//if (i == 10)
+		//	break;
 	}
+}
 
+// std::map<std::string, std::string>	creatMapInput(std::ifstream & inputBase)
+// {
+
+// }
+
+int main(int argc, char *argv[])
+{
+	std::map<std::string,std::string>data;
+	std::map<std::string, std::string>dataInput;
+	std::ifstream excelBase;
+	std::ifstream inputBase;
+
+	openNCheck(excelBase, inputBase, argv);
+	data = creatListMap(excelBase);
+	// dataInput = creatMapInput(inputBase);
+	printing(data);
+	std::cout << data.size() << std::endl;
 }
