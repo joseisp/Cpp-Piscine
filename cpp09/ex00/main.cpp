0ad4a17f16b00a7453ee300cbfd3fa5f96f6b749
jose <silvapeixotojoseinacio@gmail.com>
2023-05-08 22:07:52 -0300

    Quase...

--- cpp09/ex00/main.cpp
+++ cpp09/ex00/main.cpp
@@ -6,14 +6,47 @@
 /*   By: jinacio- < jinacio-@student.42sp.org.br    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2023/03/14 16:09:24 by jinacio-          #+#    #+#             */
-/*   Updated: 2023/04/23 13:22:22 by jinacio-         ###   ########.fr       */
+/*   Updated: 2023/05/08 22:07:42 by jinacio-         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "BitcoinExchange.hpp"
 #include <iomanip>
+#include <string.h>
+#include <string>
 
-std::map<std::string, std::string> creatListMap(std::ifstream & excelBase)
+std::string	deleteSpace(std::string src, int frontEnd)
+{
+	int i = 0;
+	int j = 1;
+	std::string ret;
+	std::string aux;
+
+	if(frontEnd == 1) // end space
+	{
+		while(src[i] != ' ')
+		{
+			aux = src[i];
+			ret.append(aux);
+			i++;
+		}
+	}
+	if (frontEnd == 2) // front space
+	{
+		while (src[j] != '\0')
+		{
+			aux = src[j];
+			ret.append(aux);
+			j++;
+		}
+	}
+
+	std::cout << ret << std::endl;
+	return ret;
+}
+
+
+std::map<std::string, std::string> creatListMap(std::ifstream & excelBase, char delimiter)
 {
 	std::map<std::string,std::string>data;
 	std::string ifs;
@@ -28,7 +61,7 @@ std::map<std::string, std::string> creatListMap(std::ifstream & excelBase)
 		for (int i = 0; i < 2; i++)
 		{
 			std::string sub;
-			getline(test, sub, ',');
+			getline(test, sub, delimiter);
 			if (i % 2 == 0)
 			{
 				pairs = sub;
@@ -46,20 +79,22 @@ std::map<std::string, std::string> creatListMap(std::ifstream & excelBase)
 			}
 		}
 	}
-	data.erase("date");
+	if (delimiter == ',')
+		data.erase("date");
 	return data;
 }
 
 void openNCheck(std::ifstream & excelBase, std::ifstream & inputBase, char *argv[])
 {
 	excelBase.open("data.csv", std::ios::in);
-	if (!excelBase)
+
+	if (excelBase)
 		std::cout << "The Excel file opened successfully" << std::endl;
 	else
 		std::cout << "Error: The Excel file did not open" << std::endl;
 
 	inputBase.open(argv[1], std::ios::in);
-	if (!inputBase)
+	if (inputBase)
 		std::cout << "The input file opened successfully" << std::endl;
 	else
 		std::cout << "Error: The input file did not open." << std::endl;
@@ -74,27 +109,56 @@ void	printing(std::map<std::string,std::string> & data)
 		std::cout << "Key: " << it->first << ", Value: "
 				<< it->second << std::endl;
 		++it;
-		//i++;
-		//if (i == 10)
-		//	break;
 	}
 }
 
-// std::map<std::string, std::string>	creatMapInput(std::ifstream & inputBase)
-// {
+void	displaying(std::string pairs, std::string odd,
+		std::map<std::string,std::string> & data)
+{
+	//std::cout << pairs << "/" << odd << std::endl;
+	//std::cout << data[pairs] << std::endl;
+}
 
-// }
+void displayCount(std::ifstream & excelBase,
+		std::map<std::string,std::string> & data)
+{
+	std::string ifs;
+	std::string pairs;
+	std::string odd;
+
+	while(!excelBase.eof())
+	{
+		getline(excelBase, ifs);
+		std::stringstream test(ifs);
+		int count = 0;
+		std::string sub;
+		for (int i = 4; i < 6; i++)
+		{
+			getline(test, sub, '|');
+			if (i % 2 == 0)
+			{
+				pairs = deleteSpace(sub, 1);
+				count += 1;
+			}
+			else
+			{
+				odd = deleteSpace(sub, 2);
+				count += 1;
+			}
+		}
+		return ;
+		displaying(pairs, odd, data);
+	}
+}
 
 int main(int argc, char *argv[])
 {
 	std::map<std::string,std::string>data;
-	std::map<std::string, std::string>dataInput;
 	std::ifstream excelBase;
 	std::ifstream inputBase;
 
 	openNCheck(excelBase, inputBase, argv);
-	data = creatListMap(excelBase);
-	// dataInput = creatMapInput(inputBase);
-	printing(data);
-	std::cout << data.size() << std::endl;
+	data = creatListMap(excelBase, ',');
+	displayCount(inputBase, data);
+
 }
